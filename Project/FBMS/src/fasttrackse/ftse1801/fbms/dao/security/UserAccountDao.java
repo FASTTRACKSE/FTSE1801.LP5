82f6f3f0ac0bb3d646ff3cbf9f6edd6aeffb6e6c
Luân <ftse1801009@st.fasttrack.edu.vn>
2018-11-28 17:18:48 +0700

    update

--- Project/FBMS/src/fasttrackse/ftse1801/fbms/dao/security/UserAccountDao.java
+++
@@ -1,7 +0,0 @@
-package fasttrackse.ftse1801.fbms.dao.security;
-
-import fasttrackse.ftse1801.fbms.entity.security.UserAccount;
-
-public interface UserAccountDao {
-	public UserAccount loadUserByUsername(final String username);
-}
--- Project/FBMS/src/fasttrackse/ftse1801/fbms/service/security/UserAccountService.java
+++
@@ -1,7 +0,0 @@
-package fasttrackse.ftse1801.fbms.service.security;
-
-import fasttrackse.ftse1801.fbms.entity.security.UserAccount;
-
-public interface UserAccountService {
-	public UserAccount loadUserByUsername(final String username);
-}
