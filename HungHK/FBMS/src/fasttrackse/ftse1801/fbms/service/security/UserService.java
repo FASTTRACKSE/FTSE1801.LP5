5c8ba2db11da1156b577da7da2ff816b37d8bca9
hunghuynhkhanh <38996842+hunghuynhkhanh@users.noreply.github.com>
2018-12-01 23:46:42 +0700

    up

---
+++ HungHK/FBMS/src/fasttrackse/ftse1801/fbms/service/security/UserService.java
@@ -0,0 +1,7 @@
+package fasttrackse.ftse1801.fbms.service.security;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+
+}
